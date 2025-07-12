ef3d5797fbfe9c83d88514e559c150e6dc1b633a
Majok-Bol <bolmajok001@gmail.com>
2025-07-12 23:59:46 +0300

    added display all functionality

--- shopping_app/scripts/main.js
+++ shopping_app/scripts/main.js
@@ -1,59 +1,81 @@
 import { shoeProducts } from "./data.js";
-console.log("Products: ", shoeProducts);
+
 
 //select product container
 const productsContainer = document.getElementById("products-container");
+productsContainer.innerHTML = "";
 
 //sort items using dropdown
 const selectContainer = document.getElementById("select");
 console.log(selectContainer.value);
-const selectedCategory = selectContainer.value;
 //store options selected
 const options = ["All"];
-//add options to the array
-//select all categories
-//add them to the array
 shoeProducts.forEach((cat) => {
+
     options.push(cat.category);
-
 });
-console.log("All Categories: ", options);
-console.log("First category: ", options[0]);
-console.log("Category length: ", options.length);
-console.log(options.includes(selectedCategory));
+console.log('Options: ', options);
+//loop through each options
 
-//loop through the options array
-//assign the categories to the option element as values
+//loop through each option 
+//add its value to be the value of the option
 for (let i = 0; i < options.length; i++) {
-    //create option element
+    //create option based on each option value
     const optionElement = document.createElement("option");
-    optionElement.classList = 'option-category';
-    //attach it to the select element as a child
     selectContainer.appendChild(optionElement);
-    //add categories as textContent of option elements
+    optionElement.value = options[i];
     optionElement.textContent = options[i];
+
 }
+
+//
+
+
+//add event listener to the select container
 selectContainer.addEventListener("change", () => {
     const selectedCategory = selectContainer.value;
-    console.log(selectedCategory);
-    //filter by category
-    //return matched
-    const product = shoeProducts.filter((item) => item.category === selectedCategory);
-    console.log(product);
-    // console.log(arr);
-    productsContainer.innerHTML = "";
-    //loop through the items
-    for (let i = 0; i < product.length; i++) {
-        productsContainer.innerHTML += `
+    if (selectedCategory === 'All') {
+        //get all the products
+        //get products with category of all
+        //display them
+        shoeProducts.map((product) =>{
+                  productsContainer.innerHTML += `
     <div class="card">
-    <h2><strong>${product[i].category}</strong></h2>
-     <img src="${product[i].image !== undefined ? `${product[i].image}` : `no image`}" alt="${product[i].title}"/>
-    <p>${product[i].description}</p>
+    <h2><strong>${product.category}</strong></h2>
+     <img src="${product.image !== undefined ? `${product.image}` : `no image`}" alt="${product.title}"/>
+    <p>${product.description}</p>
    
-    <p><strong>${product[i].title}</strong></p>
-    <p><strong>${product[i].price}</strong></p>
+    <p><strong>${product.title}</strong></p>
+    <p><strong>${product.price}</strong></p>
     </>`
+        }
+        )
+    }
+    if(selectedCategory!='All'){
+        console.log(`You have selected:${selectedCategory} `)
     }
 
+    //get all the products
+    //display them
 
+
+    // if(selectedCategory!='All'){
+    //     console.log(`${selectedCategory} has been selected`);
+    // }
 });
+
+
+
+
+
+
+
+
+
+
+
+// //map the products
+// function displayProducts(products){
+//     products.map((product)=>product)
+// }
+// displayProducts(shoeProducts);
