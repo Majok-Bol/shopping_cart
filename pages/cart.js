fd116985b8a49ffb4dbfc5a9439def2835dc1d75
Majok-Bol <bolmajok001@gmail.com>
2025-07-18 00:35:08 +0300

    added payment page

--- pages/cart.js
+++ pages/cart.js
@@ -26,30 +26,23 @@ storedItems.forEach((product) => {
 cart.addEventListener("click", (e) => {
     if (e.target.classList.contains("remove-from-cart")) {
         const cart = JSON.parse(localStorage.getItem("cartItems")) || [];
-
         const removeCartId = parseInt(e.target.id);
         console.log('Remove this product: ', removeCartId);
         console.log(typeof removeCartId);
-
-
         console.log("Stored data in local Storage: ", cart);
         //filter data by the id and remove it
         const updatedCart = cart.filter((item) => item.id !== removeCartId);
         //update the UI
-
         console.log('New Data: ', updatedCart);
         //save data
         localStorage.setItem("cartItems", JSON.stringify(updatedCart));
         console.log('Updated data: ', updatedCart);
-
         const itemToRemove = e.target.closest(`.cart-items[data-id="${removeCartId}"]`);
         console.log('Item to remove by ID: ', itemToRemove);
         if (itemToRemove) {
             itemToRemove.remove();
             alert(`Item removed from Cart`);
         }
-
-
     }
     if (e.target.classList.contains("order-item")) {
         const itemId = parseInt(e.target.id);
@@ -71,7 +64,6 @@ cart.addEventListener("click", (e) => {
             const updateCartItems = cart.filter((item) => item.id !== itemId);
             localStorage.setItem("cartItems", JSON.stringify(updateCartItems));
         } else {
-
             alert("Item already on order List");
         }
 
